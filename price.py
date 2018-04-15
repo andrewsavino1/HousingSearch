e3c7877994ed4196c693f8745c82178d41274b73
Akshat <akshat1997@gmail.com>
2018-04-15 14:21:52 -0500

    Dictionary for missing prices

--- price.py
+++ price.py
@@ -1,1 +1,9 @@
+import pandas as pd
+
+file_loc = "/Users/akshatgoel/Desktop/missing_price.csv"
+
+price_list = pd.read_csv(file_loc)
+
+price_dict = dict(zip(price_list.Neighborhood, price_list.Price))
+
 
